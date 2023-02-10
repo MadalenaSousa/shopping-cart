982e12a0ebafd0295fd775ebfdd4b2e60712a64b
Madalena Sousa <madalenaantunessousa@gmail.com>
2023-02-10 10:01:49 +0000

    ...

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import React from "react";
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Typography from '@mui/material/Typography';
 import {useState} from 'react'
 import { useDispatch } from 'react-redux';
